2652791aa4e0e98be43483f31455c5fc56215546
ynkze <ynkze@users.noreply.github.com>
2021-11-19 00:09:25 +0800

    First commit

---
+++ filesLogic.php
@@ -0,0 +1,90 @@
+<?php
+// connect to the database
+$conn = mysqli_connect('localhost', 'root', '', 'file-management');
+$key = 'qkwjdiw239&&jdafweihbrhnan&^%$ggdnawhd4njshjwuuO';
+//check files in database
+$sql = "SELECT * FROM files";
+$result = mysqli_query($conn, $sql);
+$files = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//THE KEY FOR ENCRYPTION AND DECRYPTION
+
+
+// Uploads files
+if (isset($_POST['save'])) { // if save button on the form is clicked
+    // name of the uploaded file
+    $filename = $_FILES['myfile']['name'];
+
+    // destination of the file on the server
+    $destination = 'uploads/' . $filename;
+
+    // get the file extension
+    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+    // the physical file on a temporary uploads directory on the server
+    $file = $_FILES['myfile']['tmp_name'];
+    $size = $_FILES['myfile']['size'];
+
+    if (!in_array($extension, ['zip', 'pdf', 'docx'])) {
+        echo "You file extension must be .zip, .pdf or .docx";
+    } elseif ($_FILES['myfile']['size'] > 1000000) { // file shouldn't be larger than 1Megabyte
+        echo "File too large!";
+    } else {
+        // move the uploaded (temporary) file to the specified destination
+        if (move_uploaded_file($file, $destination)) {
+            $filename = encryptthis($filename, $key);
+            $sql = "INSERT INTO files (name, size, downloads) VALUES ('$filename', $size, 0)";
+            if (mysqli_query($conn, $sql)) {
+                echo "File uploaded successfully";
+            }
+        } else {
+            echo "Failed to upload file.";
+        }
+    }
+}
+
+// Downloads files
+if (isset($_GET['file_id'])) {
+    $id = $_GET['file_id'];
+
+    // fetch file to download from database
+    $sql = "SELECT * FROM files WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+
+    $file = mysqli_fetch_assoc($result);
+    $file['name'] = decryptthis($file['name'], $key);
+    $filepath = 'uploads/' . $file['name'];
+
+    if (file_exists($filepath)) {
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header('Content-Disposition: attachment; filename=' . basename($filepath));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize('uploads/' . $file['name']));
+        readfile('uploads/' . $file['name']);
+
+        // Now update downloads count
+        $newCount = $file['downloads'] + 1;
+        $updateQuery = "UPDATE files SET downloads=$newCount WHERE id=$id";
+        mysqli_query($conn, $updateQuery);
+        exit;
+    }
+
+}
+
+//ENCRYPT FUNCTION
+function encryptthis($data, $key) {
+$encryption_key = base64_decode($key);
+$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+$encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
+return base64_encode($encrypted . '::' . $iv);
+}
+
+//DECRYPT FUNCTION
+function decryptthis($data, $key) {
+$encryption_key = base64_decode($key);
+list($encrypted_data, $iv) = array_pad(explode('::', base64_decode($data), 2),2,null);
+return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
+}
